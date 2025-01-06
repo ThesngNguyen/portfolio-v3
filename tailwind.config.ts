1509c30ecfa8abcf722011864f8468dde9ed5613
Nguyễn Cao Thắng <thangnc@whammytech.com>
2025-01-06 10:07:47 +0700

    doc: Add library, tailwind config, variants and global css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,40 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '15px'
+      }
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    backgroundImage: {
+      explosion: 'url("bg-explosion.png")',
+      circles: 'url("bg-circles.png")',
+      circleStar: 'url("/circle-star.svg")',
+      site: 'url("/site-bg.svg")',
+    },
+    animation: {
+      'spin-slow': 'spin 6s linear infinite'
+    },
+    fontFamily: {
+      sora: [`var(--font-sora)`, 'sans-serif']
+    },
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
+  },
+  container: {
+    padding: {
+      DEFAULT: '15px',
+    }
   },
   plugins: [],
 } satisfies Config;
